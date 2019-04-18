14f41cc400ed642378df8912effde5091f39108d
Jason <jw5089@nyu.edu>
2019-04-18 09:46:00 -0400

    javalert

---
+++ day20/script.js
@@ -0,0 +1,23 @@
+function totalp(){
+    var paragraphs = document.getElementsByTagName('p');
+    var plength = paragraphs.length;
+    var pstring = "There are "+plength+" paragraphs on the page";
+    console.log(pstring);
+    alert(pstring);
+}
+
+function totaldiv1(){
+    var pdiv1 = document.getElementById('div1').getElementsByTagName('p');
+    var div1length = pdiv1.length;
+    var div1string = "There are "+div1length+" paragrahs in the first ID";
+    console.log(div1string);
+    alert(div1string);
+}
+
+function totaldiv2(){
+    var pdiv2 = document.getElementById('div2').getElementsByTagName('p');
+    var div2length = pdiv2.length;
+    var div2string = "There are "+div2length+" paragrahs in the Second ID";
+    console.log(div2string);
+    alert(div2string);
+}
