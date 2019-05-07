b9ca216b24fe818b5b12a2efa9ed870aeab38bdd
Jason <jw5089@nyu.edu>
2019-05-07 03:43:13 -0400

    first commit

---
+++ final_project/script.js
@@ -0,0 +1,15 @@
+
+var modal = document.getElementById('myModal');
+
+var img = document.getElementById('myi');
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = this.src;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+      modal.style.display = "none";
+}
